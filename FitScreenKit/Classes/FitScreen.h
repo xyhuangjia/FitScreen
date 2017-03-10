17b2c1eb0438c417732916558f2de9d464b5ced9
DeepWater <2587171762@qq.com>
2017-03-10 22:51:20 +0800

    添加头文件

---
+++ FitScreenKit/Classes/FitScreen.h
@@ -0,0 +1,16 @@
+//
+//  FitScreen.h
+//  
+//
+//  Created by DeepWater on 2017/3/10.
+//
+//
+
+#ifndef FitScreen_h
+#define FitScreen_h
+
+#import "FitScreenKit.h"
+
+#define Factor(pix) [FitScreenKit shareInstance].factor*(pix)
+
+#endif /* FitScreen_h */
